2137b4cb9f3286ec715a5e1b0cd3147e140b9f19
Försth <sebastian.forsth@cgi.com>
2024-07-11 09:49:35 +0200

    add PWA support

--- steam-inventory-checker/vue.config.js
+++ steam-inventory-checker/vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+module.exports = {
+  pwa: {
+    name: "My App",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    workboxOptions: {
+      // Skip waiting and take control of the page immediately
+      skipWaiting: true,
+      clientsClaim: true,
+    },
+    manifestOptions: {
+      short_name: "MyApp",
+      start_url: ".",
+      display: "standalone",
+      background_color: "#ffffff",
+    },
+  },
+};
